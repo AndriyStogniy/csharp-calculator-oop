f3eaf8fbfb35dba680fd4e5d7855943746d5c82b
Andrii Stohnii <stogniy.9797@gmail.com>
2025-11-02 20:19:19 +0200

    feat: add base calculator

---
+++ Calculator/CalculatorLib/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public double LastResult { get; private set; }
+
+        public double Add(double a, double b)
+        {
+            LastResult = a + b;
+            return LastResult;
+        }
+        public double Substract(double a, double b)
+        {
+            LastResult = a - b;
+            return LastResult;
+        }
+        public double Multiply(double a, double b)
+        {
+            LastResult = a * b;
+            return LastResult;
+        }
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Division by zero is impossible");
+            LastResult = a / b;
+            return LastResult;
+        }
+    }
+}
