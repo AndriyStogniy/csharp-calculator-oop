c49b43517e3734b0ab350512c7115c445c8115fb
Andrii Stohnii <stogniy.9797@gmail.com>
2025-11-05 18:52:43 +0200

    feat: add MoneyAndGoodsApp

---
+++ Calculator/MoneyAndGoodsLib/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace MoneyAndGoodsLib
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public Money Price { get; set; }
+
+        public Product(string productName, Money price)
+        {
+            ProductName = productName;
+            Price = price;
+        }
+        public void IncreasePrice(Money amount)
+        {
+            Price.Increase(amount);
+        }
+        public void DecreasePrice(Money amount)
+        {
+            Price.Decrease(amount);
+        }
+
+        public override string ToString()
+        {
+            return 
+                $"Name: {ProductName}\n" + 
+                $"Price {Price.ToString()}";
+        }
+    }
+}
