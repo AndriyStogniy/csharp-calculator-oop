071cc1b254eb3336e74cc192ca5a341f264cc449
Andrii Stohnii <stogniy.9797@gmail.com>
2025-11-05 20:27:48 +0200

    feat: add DecimalNumberApp

---
+++ Calculator/DecimalNumberApp/DecimalNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalNumberApp
+{
+    public struct DecimalNumber
+    {
+        public int Value { get; set; }
+
+        public DecimalNumber(int value)
+        {
+            Value = value;
+        }
+
+        public string ToBinary()
+        {
+            return Convert.ToString(Value, 2);
+        }
+
+        public string ToOctal()
+        {
+            return Convert.ToString(Value, 8);
+        }
+
+        public string ToHex()
+        {
+            return Convert.ToString(Value, 16).ToUpper();
+        }
+
+        public override string ToString()
+        {
+            return $"Decimal: {Value}";
+        }
+    }
+}
